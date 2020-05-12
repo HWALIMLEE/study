c9268b6acd819512288a1010ab1ee4f7e9c277d0
HWALIMLEE <hwalim9612@gmail.com>
2020-05-12 12:35:00 +0900

    master

---
+++ keras02.py
@@ -0,0 +1,33 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+#데이터 준비
+x_train=np.array([1,2,3,4,5,6,7,8,9,10])
+y_train=np.array([1,2,3,4,5,6,7,8,9,10])
+x_test=np.array([101,102,103,104,105,106,107,108,109,110])
+y_test=np.array([101,102,103,104,105,106,107,108,109,110])
+
+#모델 구성
+model=Sequential()
+model.add(Dense(500,input_dim=1, activation='relu'))
+model.add(Dense(300))
+model.add(Dense(400))
+model.add(Dense(1,activation='relu'))
+
+#모델 요약
+model.summary()
+
+#compile과 훈련
+model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
+model.fit(x_train,y_train, epochs=100, batch_size=1, validation_data=(x_test, y_test))
+
+#평가,예측
+loss,acc=model.evaluate(x_test, y_test, batch_size=1)
+
+print("loss:",loss)
+print("acc:",acc)
+
+output=model.predict(x_test)
+print(output)#study
+
