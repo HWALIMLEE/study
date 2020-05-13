94a96339729bcb6f5d375be005e58ed8327e1279
HWALIMLEE <hwalim9612@gmail.com>
2020-05-13 14:13:56 +0900

    train,test로 pred알아내기

--- keras/keras02_book.py
+++ keras/keras02_book.py
@@ -27,5 +27,6 @@ model.fit(x_train,y_train,epochs=100,batch_size=1,validation_data=(x_test,y_test
 loss,acc=model.evaluate(x_test,y_test,batch_size=1)
 print("loss:",loss)
 print("acc:",acc)
+
 output=model.predict(x_test)
 print("결과물:\n",output)
