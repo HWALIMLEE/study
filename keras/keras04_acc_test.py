1a6594e39c3b3737e82aa1b880feea023102a74e
HWALIMLEE <hwalim9612@gmail.com>
2020-05-13 12:46:46 +0900

    loss!=metrics

--- keras/keras04_acc_test.py
+++ keras/keras04_acc_test.py
@@ -3,7 +3,7 @@ import numpy as np
 x=np.array([1,2,3,4,5,6,7,8,9,10])
 y=np.array([1,2,3,4,5,6,7,8,9,10]) 
 x_pred=np.array([11,12,13])
-#y_pred를 예측할것
+#y_pred를 예측할것!
 
 #2. 모델구성
 from keras.models import Sequential
