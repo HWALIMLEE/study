5d99b1edcc8be463cebca38518b2e7c21d52c9d2
HWALIMLEE <hwalim9612@gmail.com>
2020-05-31 00:23:32 +0900

    fashion_mnist.dnn

--- keras/keras65_fashion_dnn.py
+++ keras/keras65_fashion_dnn.py
@@ -17,25 +17,38 @@ x_test=x_test.reshape(10000,784)
 y_train=np_utils.to_categorical(y_train)
 y_test=np_utils.to_categorical(y_test)
 
+
 model=Sequential()
-model.add(Dense(20,input_shape=(784,),activation='relu'))
+model.add(Dense(20,input_shape=(784,)))
 model.add(Dense(30,activation='relu'))
 model.add(Dense(30,activation='relu'))
 model.add(Dense(30,activation='relu'))
+# model.add(Dropout(0.2))
 model.add(Dense(30,activation='relu'))
-model.add(Dense(10,activation='relu'))
+model.add(Dense(20,activation='relu'))
+# model.add(Dropout(0.3))
+model.add(Dense(30,activation='relu'))
+model.add(Dense(30,activation='relu'))
+model.add(Dense(30,activation='relu'))
+# model.add(Dense(30,activation='relu'))
+model.add(Dense(50,activation='relu'))
 model.add(Dense(30,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
 #3. 훈련
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['acc'])
-model.fit(x_train,y_train,epochs=10, batch_size=100)
+model.fit(x_train,y_train,epochs=300, batch_size=150)
 
 #4. 평가, 예측
-loss,acc=model.evaluate(x_test,y_test,batch_size=100)
+loss,acc=model.evaluate(x_test,y_test,batch_size=50)
 
 print("loss:",loss)
 print("acc:",acc)
 
 y_predict=model.predict(x_test)
 print("y_predict:",np.argmax(y_predict,axis=1))
+
+"""
+loss: 0.41
+acc: 0.87
+"""
