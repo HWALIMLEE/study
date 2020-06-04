bd0b6835eddea10b5752bed3cf9e768c04d0c1c0
HWALIMLEE <hwalim9612@gmail.com>
2020-06-04 20:53:13 +0900

    machine learning

---
+++ ml/m02_and.py
@@ -0,0 +1,26 @@
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+#1.데이터
+x_data=[[0,0],[1,0],[0,1],[1,1]]
+y_data=[0,0,0,1]
+#and
+
+#2.모델
+model=LinearSVC()
+
+#3.실행
+model.fit(x_data,y_data)
+
+#4.평가예측
+x_test = [[0,0],[1,0],[0,1],[1,1]]
+y_predict = model.predict(x_test)
+
+acc=accuracy_score([0,0,0,1],y_predict)
+#그냥 score는 evaluate와 동일한 것
+#evaluate대신 score사용
+
+print(x_test,"의 예측 결과:",y_predict)
+print("acc=",acc)
+
+#이 구성이 xgboost까지 간다
