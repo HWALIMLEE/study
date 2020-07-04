0b4fcea7f0ad24afe1bac241c51ff5c5b57f7e38
HWALIMLEE <hwalim9612@gmail.com>
2020-07-05 00:22:21 +0900

    homework_BatchNormalization

---
+++ homework/handson/p421_BatchNormalization.py
@@ -0,0 +1,54 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, LeakyReLU,BatchNormalization
+from keras.datasets import fashion_mnist
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+
+(x_train_full,y_train_full), (x_test,y_test) = fashion_mnist.load_data()
+
+x_valid,x_train = x_train_full[:5000]/255.0, x_train_full[5000:] / 255.0
+y_valid,y_train = y_train_full[:5000],y_train_full[5000:]
+x_test = x_test/255.0
+
+
+model = Sequential()
+model.add(Flatten(input_shape=(28,28)))
+model.add(BatchNormalization())
+model.add(Dense(300,activation='elu',kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Dense(100,activation='elu', kernel_initializer='he_normal'))
+model.add(LeakyReLU(alpha=0.2))
+model.add(BatchNormalization())
+model.add(Dense(10,activation='softmax'))
+
+model.summary()
+
+
+checkpoint = ModelCheckpoint("./model/my_keras_model.h5")
+
+model.compile(loss='sparse_categorical_crossentropy',
+                optimizer=Adam(learning_rate=0.01),metrics=['acc'])
+hist = model.fit(x_train,y_train,epochs=30,
+                validation_data=(x_valid,y_valid),callbacks=[checkpoint])
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# pd.DataFrame(hist.history).plot(figsize=(8,5))
+# plt.grid()
+# plt.gca().set_ylim(0,1)
+# plt.show()
+
+loss,acc = model.evaluate(x_test,y_test)
+print("loss:",loss)
+print("acc:",acc)
+
+
+###BatchNormalization 쓴 후###
+# loss: 0.37233526463508604
+# acc: 0.892300009727478
+# 쓰고 나니 더 좋아졌다
