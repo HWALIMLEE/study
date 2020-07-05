b5dc4ce67793ca103183ba36095bf452af1c52ea
HWALIMLEE <hwalim9612@gmail.com>
2020-07-05 16:36:19 +0900

    homework

--- homework/handson/p425_BatchNoramlization2.py
+++ homework/handson/p425_BatchNoramlization2.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
 from keras.models import Sequential
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, LeakyReLU,BatchNormalization, Activation
 from keras.datasets import fashion_mnist
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
@@ -13,18 +13,21 @@ x_valid,x_train = x_train_full[:5000]/255.0, x_train_full[5000:] / 255.0
 y_valid,y_train = y_train_full[:5000],y_train_full[5000:]
 x_test = x_test/255.0
 
-print(x_train.shape) #(55000,28,28)
-print(x_valid.shape) #(5000,28,28)
-print(x_test.shape)  #(10000,28,28)
-print(y_train.shape) #(55000,)
 
 model = Sequential()
-model.add(Flatten(input_shape=[28,28]))
-model.add(Dense(300,activation='relu'))
-model.add(Dense(100,activation='relu'))
+model.add(Flatten(input_shape=(28,28)))
+model.add(BatchNormalization())
+model.add(Dense(300,kernel_initializer='he_normal',use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation('elu'))
+model.add(Dense(100, kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation('elu'))  # model.add(Activation='elu') (x)
 model.add(Dense(10,activation='softmax'))
 
 model.summary()
+
+# 배치 정규화 층은 입력마다 이동 파라미터를 포함하기 때문에 이전 층에서 편향을 뺄 수 있습니다. 
 
 
 checkpoint = ModelCheckpoint("./model/my_keras_model.h5")
@@ -38,7 +41,16 @@ hist = model.fit(x_train,y_train,epochs=30,
 import pandas as pd
 import matplotlib.pyplot as plt
 
-pd.DataFrame(hist.history).plot(figsize=(8,5))
-plt.grid()
-plt.gca().set_ylim(0,1)
-plt.show()
+# pd.DataFrame(hist.history).plot(figsize=(8,5))
+# plt.grid()
+# plt.gca().set_ylim(0,1)
+# plt.show()
+
+loss,acc = model.evaluate(x_test,y_test)
+print("loss:",loss)
+print("acc:",acc)
+
+
+###BatchNormalization 쓴 후###
+# loss: 0.41138803104162214
+# acc: 0.8873000144958496
